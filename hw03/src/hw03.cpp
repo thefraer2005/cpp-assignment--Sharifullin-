4bb27b8534adf647167ab56905e8da52eec0c95e
ARHTUR <sharifullinartur509@gmail.com>
2024-11-28 23:12:13 +0500

    hw03
    Signed-off-by: ARHTUR <sharifullinartur509@gmail.com>

--- hw03/src/hw03.cpp
+++ hw03/src/hw03.cpp
@@ -6,12 +6,12 @@ void hw03(char* str, const char* pattern) {
     if (str == nullptr||pattern ==nullptr) {
         return; 
     }
-    if (*str=='0') {
+     if (*str == '\0' || *pattern == '\0') {  
         return; 
     }
     int strLen = std::strlen(str);
     int patternLen = std::strlen(pattern);
-    for (size_t i = 0; i <= strLen - patternLen; ++i) {
+    for (int i = 0; i <= strLen - patternLen; ++i) {
       if (std::strncmp(&str[i], pattern, patternLen) == 0) {
         std::memmove(&str[i],&str[i + patternLen],strLen -i-patternLen+ 1);
         break; 
